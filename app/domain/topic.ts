29b825bf52b36ba841f358415b8421907c41ab22
Stefanos Gatsios <stevengatsios@gmail.com>
2017-04-28 16:28:39 +0300

    CheckBoxes functioning (?)

--- app/domain/topic.ts
+++ app/domain/topic.ts
@@ -2,10 +2,15 @@
  * Created by stefania on 4/26/17.
  */
 export interface Topic {
-    _id: number;
+    _id: string;
     name: string;
     description: string;
     date : Date;
     weight: number;
     questionOrder: string;
+}
+
+export interface CheckTopic {
+    topic : Topic;
+    checked : boolean;
 }
--- app/pages/faq/topics.components.ts
+++ app/pages/faq/topics.components.ts
@@ -1,10 +1,10 @@
 /**
  * Created by stefania on 4/26/17.
  */
-import { Component, ViewChild } from '@angular/core';
+import {Component, ViewChild, OnInit} from '@angular/core';
 import { ModalDirective } from 'ngx-bootstrap/modal';
 import { Router } from '@angular/router';
-import { Topic } from "../../domain/topic";
+import {Topic, CheckTopic} from "../../domain/topic";
 import { FAQService } from "../../services/faq.service";
 import {FormGroup, FormBuilder, Validators} from "@angular/forms";
 
@@ -13,19 +13,19 @@ import {FormGroup, FormBuilder, Validators} from "@angular/forms";
     templateUrl: './topics.component.html',
 })
 
-export class TopicsComponent {
+export class TopicsComponent implements OnInit {
 
     @ViewChild('autoShownModal') public autoShownModal:ModalDirective;
     public isModalShown:boolean = false;
 
     public topics: Topic[];
+    public topicsCheckboxes : CheckTopic[] = [];
     public errorMessage: string;
 
     public formGroup : FormGroup;
 
     ngOnInit() {
         this.getTopics();
-
     }
 
     constructor(private _router: Router,
@@ -33,14 +33,20 @@ export class TopicsComponent {
                 private _fb: FormBuilder) {}
 
     getTopics() {
+        let self = this;
         this._faqService.getTopics().subscribe(
-            topics => this.topics = topics,
+            topics => {
+                topics.forEach(_ => {
+                    console.log(_);
+                    self.topicsCheckboxes.push(<CheckTopic>{topic : _, checked : false});
+                });
+            },
             error =>  this.errorMessage = <any>error);
     }
 
     public showModal(mode : string):void {
         this.isModalShown = true;
-
+        console.log(this.getSelectedTopics());
         this.formGroup = this._fb.group({
             name : ['', Validators.required],
             description : ['', Validators.required],
@@ -58,8 +64,15 @@ export class TopicsComponent {
     }
 
     public toggleCheckBoxes(event) {
-        this.topics.forEach(x => x.state = event.target.checked);
-        // console.log(event.target.checked);
+        this.topicsCheckboxes.forEach(_ => _.checked = event.target.checked);
+    }
+
+    public applyCheck(flag : boolean) {
+        this.topicsCheckboxes.forEach(_ => _.checked = flag);
+    }
+
+    public getSelectedTopics() : Topic[] {
+        return this.topicsCheckboxes.filter(topic => topic.checked == true).map(checkedTopic => checkedTopic.topic);
     }
 
     public saveTopic(data : Topic):void {
@@ -70,8 +83,9 @@ export class TopicsComponent {
     }
 
     public topicSavedSuccessfully(topic: Topic) {
-        this.topics.push(topic);
+        this.topicsCheckboxes.push(<CheckTopic>{topic : topic, checked : false});
         this.hideModal();
+        this.applyCheck(false);
     }
 
     handleError(error) {
