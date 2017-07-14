01a0ef00419b70dd628346afb738c38c7a32051f
smartziou <stefania.martziou@imis.athena-innovation.gr>
2017-07-14 12:26:25 +0300

    WIP help content - get page help content for edit page

--- app/pages/helpcontent/edit-page-help-content.component.ts
+++ app/pages/helpcontent/edit-page-help-content.component.ts
@@ -3,6 +3,9 @@
  */
 import { Component, ViewChild } from '@angular/core';
 import { PageContentFormComponent } from "./page-help-content-form.component";
+import { Subscription } from "rxjs/Subscription";
+import { HelpContentService } from "../../services/help-content.service";
+import { PageHelpContent } from "../../domain/page-help-content";
 
 @Component({
     selector: 'edit-page-help-content',
@@ -14,4 +17,29 @@ export class EditPageHelpContentComponent {
     @ViewChild(PageContentFormComponent)
     public formComponent : PageContentFormComponent;
 
+    private sub: Subscription;
+
+    private pageHelpContent: PageHelpContent;
+
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private _helpContentService: HelpContentService) {}
+
+    ngOnInit() {
+
+        this.sub = this.route.params.subscribe(params => {
+            let id = params['id'];
+            this._helpContentService.getPageHelpContents(id).subscribe(
+                pageHelpContent => this.pageHelpContent = pageHelpContent,
+                error => this.handleError('System error retrieving page help content', error));
+        });
+    }
+    ngOnDestroy() {
+        this.sub.unsubscribe();
+    }
+
+    handleError(message: string, error) {
+        this.errorMessage = message + ' (Server responded: ' + error + ')';
+    }
 }
